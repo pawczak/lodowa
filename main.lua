945ff9fe363cabc692993f2e3533536a5eb5f6c3
pawczak <grzyb000>
2015-07-07 22:49:44 +0200

    added simple photo capture

--- main.lua
+++ main.lua
@@ -6,11 +6,24 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-local catchRect = display.newRect()
+local media = require("media")
+local properties = require("tools.properties")
+local captureImg
+
+local catchRect = display.newRect(properties.center.x, properties.center.y, 200, 200)
+
+local function onComplete(event)
+    captureImg = event.target
+    print("photo w,h = " .. captureImg.width .. "," .. captureImg.height)
+end
 
 local function catchRectListener(event)
     if event.phase == "ended" then
-        --TODO: catch that shit
+        if media.hasSource(media.Camera) then
+            media.capturePhoto({ listener = onComplete })
+        else
+            native.showAlert("Corona", "This device does not have a camera.", { "OK" })
+        end
     end
 end
 
---
+++ tools/properties.lua
@@ -0,0 +1,7 @@
+local properties = {}
+
+properties.x = display.screenOriginX
+properties.y = display.screenOriginY
+properties.center = { x = display.contentWidth * 0.5, y = display.contentHeight * 0.5}
+
+return properties
